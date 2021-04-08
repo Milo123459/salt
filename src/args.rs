c96e14ca86c1606438e5eb329240a282d948f207
Milo <50248166+Milo123459@users.noreply.github.com>
2021-04-08 18:33:05 +0100

    chore: style: run fmt

--- src/args.rs
+++ src/args.rs
@@ -6,11 +6,14 @@ pub struct Arguments {
 	/// type of action. run the `action` / `actions` action to see available actions.
 	pub action: String,
 
-    /// arguments to action
+	/// arguments to action
 	pub arguments: Vec<String>,
 
-    /// todo node name
-    #[structopt(long, short, default_value="Default")]
-    pub node: String
+	/// todo node name
+	#[structopt(long, short, default_value = "Default")]
+	pub node: String,
 
+	/// display done tasks (only applies on certain commands)
+	#[structopt(long, short)]
+	pub checked: bool,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+mod args;
+mod cli;
 mod config;
 mod get_and_parse;
 mod macros;
 mod node;
 mod task;
-mod cli;
-mod args;
 
 /*
 This file allows us to use `use crate::module` in any file in src/
