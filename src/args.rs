9f3b2d74371139b432d590b58858da2c0d0fa704
Milo <50248166+Milo123459@users.noreply.github.com>
2021-04-10 17:19:28 +0100

    chore: workflows: revert useless stuff

--- src/args.rs
+++ src/args.rs
@@ -1,5 +1,5 @@
-use structopt::StructOpt;
 use serde::{Deserialize, Serialize};
+use structopt::StructOpt;
 
 #[derive(Serialize, Deserialize, Debug, StructOpt, PartialEq, Clone)]
 pub struct Arguments {
