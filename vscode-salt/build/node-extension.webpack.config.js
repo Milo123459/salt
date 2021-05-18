c1c6a3cf1a4d6c9242820e8abfd46bac73c4dc68
Milo <50248166+Milo123459@users.noreply.github.com>
2021-05-18 10:23:12 +0100

    feat: extension: start of webviews

--- vscode-salt/build/node-extension.webpack.config.js
+++ vscode-salt/build/node-extension.webpack.config.js
@@ -12,14 +12,13 @@ const config = {
   entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "..", "dist"),
     filename: "extension.js",
     libraryTarget: "commonjs2",
   },
   devtool: "nosources-source-map",
   externals: {
     vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
-    // modules added here also need to be added in the .vsceignore file
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
